3284ce2cd99527f0a8f0cc487af3cdc5ecc4894b
MariiaMerkel <mariia_merkel@mail.ru>
2023-12-25 21:43:50 +0300

    добавила фигурных скобочек

--- src/Main.java
+++ src/Main.java
@@ -14,32 +14,50 @@ public class Main {
 
         System.out.println("task 2");
         for (int i = 0; i < integers.length; i++) {
-            if (i < integers.length - 1) System.out.print(integers[i] + ", ");
-            else System.out.println(integers[i]);
+            if (i < integers.length - 1) {
+                System.out.print(integers[i] + ", ");
+            } else {
+                System.out.println(integers[i]);
+            }
         }
 
         for (int i = 0; i < doubles.length; i++) {
-            if (i < doubles.length - 1) System.out.print(doubles[i] + ", ");
-            else System.out.println(doubles[i]);
+            if (i < doubles.length - 1) {
+                System.out.print(doubles[i] + ", ");
+            } else {
+                System.out.println(doubles[i]);
+            }
         }
 
         for (int i = 0; i < chars.length; i++) {
-            if (i < chars.length - 1) System.out.print(chars[i] + ", ");
-            else System.out.println(chars[i]);
+            if (i < chars.length - 1) {
+                System.out.print(chars[i] + ", ");
+            } else {
+                System.out.println(chars[i]);
+            }
         }
 
         System.out.println("task 3");
         for (int i = integers.length - 1; i >= 0; i--) {
-            if (i > 0) System.out.print(integers[i] + ", ");
-            else System.out.println(integers[i]);
+            if (i > 0) {
+                System.out.print(integers[i] + ", ");
+            } else {
+                System.out.println(integers[i]);
+            }
         }
         for (int i = doubles.length - 1; i >= 0; i--) {
-            if (i > 0) System.out.print(doubles[i] + ", ");
-            else System.out.println(doubles[i]);
+            if (i > 0) {
+                System.out.print(doubles[i] + ", ");
+            } else {
+                System.out.println(doubles[i]);
+            }
         }
         for (int i = chars.length - 1; i >= 0; i--) {
-            if (i > 0) System.out.print(chars[i] + ", ");
-            else System.out.println(chars[i]);
+            if (i > 0) {
+                System.out.print(chars[i] + ", ");
+            } else {
+                System.out.println(chars[i]);
+            }
         }
 
         System.out.println("task 4");
